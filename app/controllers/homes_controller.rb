663ee76962f0c367492bec2f8e0441158dd2fa8e
EC2 Default User <ec2-user@ip-172-31-60-68.ec2.internal>
2020-11-26 10:07:17 +0000

    PF_1126

--- app/controllers/homes_controller.rb
+++ app/controllers/homes_controller.rb
@@ -1,12 +1,17 @@
 class HomesController < ApplicationController
-  
+
   def top
+    @infolists = Infolist.all
+    @infolist = Infolist.new
+    @random = Infolist.order("RANDOM()").limit(5)
+
+
   end
-  
+
   def about
   end
-  
+
   def qa
   end
-  
+
 end
--- app/controllers/infolists_controller.rb
+++ app/controllers/infolists_controller.rb
@@ -8,18 +8,29 @@ class InfolistsController < ApplicationController
     @infolist = Infolist.new
   end
 
+  # def create
+  #   # １. データを新規登録するためのインスタンス作成
+  #   @infolist = Infolist.new(infolist_params)
+  #   # ２. データをデータベースに保存するためのsaveメソッド実行
+  #   @infolist.save
+  #   # ３. トップ画面へリダイレクト
+  #   redirect_to infolists_path(id: current_user)
+  # end
   def create
-    # １. データを新規登録するためのインスタンス作成
     @infolist = Infolist.new(infolist_params)
-    # ２. データをデータベースに保存するためのsaveメソッド実行
-    @infolist.save
-    # ３. トップ画面へリダイレクト
-    redirect_to infolists_path(id: current_user)
+    @infolist.user_id = current_user.id
+    if @infolist.save
+      redirect_to infolist_path(@infolist), notice: "You have created book successfully."
+    else
+      @infolists = Infolist.all
+      render 'index'
+    end
   end
 
   def index
     # @lists = List.all
     @infolists = Infolist.all
+    @infolist = Infolist.new
   end
 
   def show
@@ -32,15 +43,26 @@ class InfolistsController < ApplicationController
     @infolist = Infolist.find(params[:id])
   end
 
+  # def update
+  #   infolist = Infolist.find(params[:id])
+  #   infolist.update(infolist_params)
+  #   redirect_to infolist_path(infolist.id)
+  # end
+
   def update
-    infolist = Infolist.find(params[:id])
-    infolist.update(infolist_params)
-    redirect_to infolist_path(infolist.id)
+    @infolist = Infolist.find params[:id]
+    if @infolist.update(infolist_params)
+      redirect_to infolist_path(@infolist), notice: "You have updated  successfully."
+    else
+      render "edit"
+    end
   end
 
+
+
   def destroy
-    infolist = Infolist.find(params[:id])  # データ（レコード）を1件取得
-    infolist.destroy  # データ（レコード）を削除
+    @infolist = Infolist.find(params[:id])  # データ（レコード）を1件取得
+    @infolist.destroy  # データ（レコード）を削除
     redirect_to infolists_path  # 投稿一覧画面へリダイレクト
   end
 
